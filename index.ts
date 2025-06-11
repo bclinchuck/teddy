d473109ca520662ba9468f65d229e226aa4f9ca9
bclinchuck <bclinchuck@gmail.com>
2025-06-11 10:05:42 -0400

    fixed

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
-//const _ = require("lodash")
-//console.log(_.toUpper("Hello Brandon"))
 import express, {Express, Request, Response} from "express";
-const port = 3000;
 
+const port = 3000;
 const app: Express = express();
 
-app.get("/ping", (req: Request, res: Response) => {
-    res.status(200).send("pong");
-});
+app.get("/ping", (req, res) => {
+    res.status(200).send("pongy");
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
