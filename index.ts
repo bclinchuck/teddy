ae9e77225fc4017d0a2d9aabf492e9bd03942d09
bclinchuck <bclinchuck@gmail.com>
2025-06-11 10:05:53 -0400

    fixed

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@ const port = 3000;
 const app: Express = express();
 
 app.get("/ping", (req, res) => {
-    res.status(200).send("pongy");
-})
+    res.status(200).send("pong");
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
