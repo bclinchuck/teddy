3c9b4117073d0cdf947a33579a420f2b4fd4a340
bclinchuck <bclinchuck@gmail.com>
2025-06-11 10:05:42 -0400

    Fix of code test

--- index.ts
+++ index.ts
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 import express, {Express, Request, Response} from "express";
 
 const port = 3000;
 const app: Express = express();
 
 app.get("/ping", (req, res) => {
-=======
-//const _ = require("lodash")
-//console.log(_.toUpper("Hello Brandon"))
-import express, {Express, Request, Response} from "express";
-const port = 3000;
-
-const app: Express = express();
-
-app.get("/ping", (req: Request, res: Response) => {
->>>>>>> 754cdfd (ping pong request on port 3000)
     res.status(200).send("pong");
 });
 
