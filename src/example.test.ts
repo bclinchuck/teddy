024e13f37acb47ef606d2067d906f0c04f7b8b1c
bclinchuck <bclinchuck@gmail.com>
2025-06-30 12:01:17 -0400

    update

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,5 +1,5 @@
 describe("Example test suite", () => {
-    it("should pass a dummy test", () => {
-      expect(true).toBe(true);
-    });
-  });
+	it("should pass a dummy test", () => {
+		expect(true).toBe(true);
+	});
+});
