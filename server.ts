c29d1ff0637b96c6fbf36f371bab9167d5a8b308
bclinchuck <bclinchuck@gmail.com>
2025-07-21 10:00:10 -0400

    Squashed biome-linting commits

--- server.ts
+++ server.ts
@@ -1,31 +1,33 @@
-import express, { Request, Response} from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import { createServer } from "node:http";
+import path from "node:path";
+import express, { Request, Response } from "express";
+import { Server } from "socket.io";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname));
+// Serve static files from project root
+app.use(express.static(path.join(__dirname, "..")));
 
-app.get('/ping', (req, res) => {
-    res.send('pong');
+app.get("/ping", (req, res) => {
+	res.send("pong");
 });
 
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-io.on('connection', (socket) => {
-    console.log(`Connected: ${socket.id}`);
+io.on("connection", (socket) => {
+	console.log(`Connected: ${socket.id}`);
 
-    socket.on('disconnect', () => {
-        console.log(`Disconnected: ${socket.id}`);
-    });
-    socket.on('pingFromClient', () => {
-        console.log('Ping received through socket.io');
-        socket.emit('pongFromServer', 'pong');
-    })
+	socket.on("disconnect", () => {
+		console.log(`Disconnected: ${socket.id}`);
+	});
+	socket.on("pingFromClient", () => {
+		console.log("Ping received through socket.io");
+		socket.emit("pongFromServer", "pong");
+	});
 });
 
 httpServer.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+	console.log(`Server is running at http://localhost:${port}`);
 });
